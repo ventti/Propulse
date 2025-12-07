f965a3b96219023caa7775391929b9bf2b30dea0
Teppo Keitaanniemi <tep-po@iki.fi>
2025-12-07 14:27:13 +0200

    refactor cmake: move fpc compiler finding logic to cmake/fpc.cmake

--- cmake/fpc.cmake
+++ cmake/fpc.cmake
@@ -10,3 +10,31 @@ set(FPC_SEARCH_BASE_PATHS
 # Add Linux-specific paths
 set(FPC_SEARCH_BASE_PATHS_LINUX ${FPC_SEARCH_BASE_PATHS} /usr/lib/fpc)
 
+# Find FPC compiler
+# For cross-compilation, toolchain files set FPC_EXECUTABLE_NAME and FPC_SEARCH_PATHS
+# For native builds, use regular fpc compiler
+if(CMAKE_CROSSCOMPILING)
+    if(DEFINED FPC_EXECUTABLE_NAME)
+        # Use cross-compiler specified in toolchain file
+        if(DEFINED FPC_SEARCH_PATHS)
+            find_program(FPC_EXECUTABLE ${FPC_EXECUTABLE_NAME} PATHS ${FPC_SEARCH_PATHS} NO_DEFAULT_PATH)
+        endif()
+        # If not found in specified paths, try system PATH
+        if(NOT FPC_EXECUTABLE)
+            find_program(FPC_EXECUTABLE ${FPC_EXECUTABLE_NAME})
+        endif()
+    else()
+        # Toolchain file didn't set FPC_EXECUTABLE_NAME, fall back to regular fpc
+        # (This shouldn't happen if toolchain files are properly configured)
+        message(STATUS "Toolchain file didn't set FPC_EXECUTABLE_NAME, using regular fpc")
+        find_program(FPC_EXECUTABLE fpc)
+    endif()
+else()
+    # Native build: use regular fpc compiler
+    find_program(FPC_EXECUTABLE fpc)
+endif()
+
+if(NOT FPC_EXECUTABLE)
+    message(FATAL_ERROR "FreePascal Compiler (fpc) not found. Please install FPC 3.2+")
+endif()
+
